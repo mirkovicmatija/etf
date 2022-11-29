58e701c48db18d99f6a58af634e7db25d8d64d96
mirkovicmatija <matija.mirkovic003@gmail.com>
2022-11-29 11:51:29 +0100

    popravljen jedan zadatak

--- kolokvijum/dvaka.c
+++ kolokvijum/dvaka.c
@@ -9,17 +9,21 @@ int main()
     } while (k < 2);
 
     double s = -1;
-    int fakt = 1;
 
     for (int i = 2; i <= k; i++)
     {
         double p = 1;
-        for (int j = 0; j < x; j++)
+        int fakt = 1;
+        for (int j = 1; j <= x; j++)
         {
             p *= 2 * i - 1;
         }
 
-        fakt *= i * 2 - 2;
+        for (int j = 1; j < 2 * i - 1; j++)
+        {
+            fakt *= j;
+        }
+
         int jedan = i % 2 ? -1 : 1;
         s += jedan * p / fakt;
     }
