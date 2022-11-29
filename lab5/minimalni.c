c4d6f70e8754e99854fa84b7ec00769790138a1e
mirkovicmatija <matija.mirkovic003@gmail.com>
2022-11-29 13:49:44 +0100

    lab4 popravljen

--- lab5/minimalni.c
+++ lab5/minimalni.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main(){
-	int n, min = n, u, i=0;
-	scanf("%d",&n);
-	do{
-	printf("--> ");
-	scanf("%d",&u);
-	min = (u < min)? u : min;
-	i++;
-	} while(i<n);
+int main()
+{
+	int n, u, min = u, i = 0;
+	scanf("%d", &n);
+	do
+	{
+		printf("--> ");
+		scanf("%d", &u);
+		min = (u < min) ? u : min;
+		i++;
+	} while (i < n);
 	printf("Najmanj broj je %d.", min);
 	return 0;
 }
-
